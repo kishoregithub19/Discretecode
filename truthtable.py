871241aae23265ae7d43f6851488536cd7333cff
Kishore Kumar P. K <KishorePK@act.edu.om>
2021-09-23 09:52:10 +0400

    Both added

---
+++ truthtable.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 29 14:47:02 2019
+
+@author: kishorePK
+"""
+import numpy as np
+
+X = np.array[(5)
+Y = np.array(5)
+for i in len(X):
+    for j in len(Y):
+        print(X[j] or y[j])
